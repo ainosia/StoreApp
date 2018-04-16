c525906811388ca0634b037eea0964f4a8a0ae96
Sonia Villa <ainosia@gmail.com>
2018-04-16 18:43:17 +0200

    Se cambia el entorno a producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDu34lvOCoj52YmOqP6x5pBerpP8G6iwz4",
+    authDomain: "storeapp-c5867.firebaseapp.com",
+    databaseURL: "https://storeapp-c5867.firebaseio.com",
+    projectId: "storeapp-c5867",
+    storageBucket: "storeapp-c5867.appspot.com",
+    messagingSenderId: "492220283825"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyDu34lvOCoj52YmOqP6x5pBerpP8G6iwz4",
-    authDomain: "storeapp-c5867.firebaseapp.com",
-    databaseURL: "https://storeapp-c5867.firebaseio.com",
-    projectId: "storeapp-c5867",
-    storageBucket: "storeapp-c5867.appspot.com",
-    messagingSenderId: "492220283825"
-  }
+  production: true
 };
